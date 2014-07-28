b62bf373766c9374192e9443a493707d2515fbf0
Alejandro Lopez <alejjx@gmail.com>
2014-07-27 17:05:33 -0700

    made makefile and file for problem 6.1 in practice book

---
+++ arrays/p6_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+void rearrange(int A[], int index);
+
+int main(void){
+    int myArray[9] = {1, 6, 9, 4, 7, 2, 4, 5, 11};
+    rearrange(myArray, 3);
+}
+
+void rearrange(int A[], int index){
+    
+}
