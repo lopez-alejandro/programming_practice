a6dfd5ba1739d84701eb9351c0d8db779f550e1c
Alejandro Lopez <alejjx@gmail.com>
2014-07-29 21:53:42 -0700

    could not solve problem. ended up using solution from text but at least i understand how the solution works. it's quite ingenious

--- arrays/p6_1.cpp
+++ arrays/p6_1.cpp
@@ -1,12 +1,44 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
-void rearrange(int A[], int index);
+void rearrange(vector<int>* A, int index);
 
 int main(void){
-    int myArray[9] = {1, 6, 9, 4, 7, 2, 4, 5, 11};
-    rearrange(myArray, 3);
+    vector<int> A; 
+    A.push_back(1);
+    A.push_back(6);
+    A.push_back(4);
+    A.push_back(7);
+    A.push_back(2);
+    A.push_back(4);
+    A.push_back(5);
+    A.push_back(11);
+    rearrange(&A, 2);
 }
 
-void rearrange(int A[], int index){
-    
+void rearrange(vector<int>* A, int index){
+    int pivot = (*A)[index];   
+    /*
+     * keep three smaller subarrays.
+     * bottom: [0, smaller-1]
+     * middle: [smaller, equal -1]
+     * top: [larger+1, size -1]
+     */
+    int smaller = 0, equal = 0, larger = A->size()-1;
+
+    while(equal <= larger){
+        if((*A)[equal] < pivot){
+            swap((*A)[smaller++], (*A)[equal++]);
+        } else if((*A)[equal] == pivot){
+            ++equal;
+        } else{
+            swap((*A)[equal], (*A)[larger--]);
+        }
+    }
+
+    for(vector<int>::iterator it = A->begin(); it!= A->end(); ++it){
+        cout << *it << " ";
+    }
+    cout <<endl;
 }
